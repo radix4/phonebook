eb1387e3d7472973c6f8744aec06c941c29ecc70
Thang Cao <shiyoline@gmail.com>
2021-01-02 15:33:17 -0800

    update app with adding names functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
-function App() {
-  return <div>Empty...</div>
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Thang Cao' },
+    { name: 'Clever Charlotte' },
+  ])
+  const [newName, setNewName] = useState('') //newName is for controlling the form input element
+
+  const addPerson = event => {
+    event.preventDefault()
+    console.log('button  clicked', event.target)
+
+    const personObject = {
+      name: newName,
+      id: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = event => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+          <button type='submit'>add</button>
+        </div>
+      </form>
+
+      <div>debug: {newName}</div>
+
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => (
+          <div key={person.name}>{person.name}</div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
