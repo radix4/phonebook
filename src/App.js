0e27e5fa94367ac138657c1bb1c5c041bb0e2ef3
Thang Cao <shiyoline@gmail.com>
2021-01-03 23:02:54 -0800

    users can add phone numbers

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Thang Cao', id: 'Thang Cao' },
-    { name: 'Clever Charlotte', id: 'Clever Charlotte' },
+    { name: 'Thang Cao', number: '15531232314', id: 'Thang Cao' },
+    { name: 'Machine', number: '101010001001', id: 'Machine' },
   ])
   const [newName, setNewName] = useState('') //newName is for controlling the form input element
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = event => {
     event.preventDefault()
 
     const personObject = {
       name: newName,
+      number: newNumber,
       id: newName,
     }
 
@@ -30,21 +32,33 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = event => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
 
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          <button type='submit'>add</button>
+          <div>
+            name: <input value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            number: <input value={newNumber} onChange={handleNumberChange} />
+          </div>
+          <div>
+            <button type='submit'>add</button>
+          </div>
         </div>
       </form>
 
       <h2>Numbers</h2>
       <div>
         {persons.map(person => (
-          <Person key={person.name} name={person.name} />
+          <Person key={person.name} name={person.name} number={person.number} />
         ))}
       </div>
     </div>
